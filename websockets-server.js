5147fb6233861946b0dac08b296335b04ab621fc
Huy Dao <huydao200790@gmail.com>
2018-03-19 15:01:42 -0700

    first section

---
+++ websockets-server.js
@@ -0,0 +1,31 @@
+var WebSocket = require('ws');
+var WebSocketServer = WebSocket.Server;
+var port = 3001;
+var ws = new WebSocketServer({
+  port: port
+});
+var messages = [];
+var topic = {};
+
+console.log('websocketes server started');
+
+ws.on('connection',function(socket){
+  console.log('client connection established');
+
+  messages.forEach(function(msg){
+    socket.send(msg);
+  });
+  socket.on('message',function(data){
+    console.log('message received: '+data);
+  //  if(data == '/topic')
+  //    topic = 'this is new chat';
+  //  else
+    messages.push(data);
+
+    ws.clients.forEach(function(clientSocket){
+    // clientSocket.send(topic);
+      clientSocket.send(data);
+    });
+  //  socket.send(data);
+  });
+});
